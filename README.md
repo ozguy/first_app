9a636e7ca25fe5def505281d1ea4f65635f6130e
Alan CHoyna <achoyna@gmail.com>
2013-02-20 16:45:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby on Rails tutorial
+
+This is my first app in Ruby.
+
+My web site is [*www.choyna.com*](http://www.choyna.com)
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '334f679ab7472968b9211ce307bd6987f2ef616a8d5e01df28d5fe9c7002d84867b50f25ad778c07e80a0ea423e56d05aaefbc61c3c9d9767e35c4d0ab3119b2'
